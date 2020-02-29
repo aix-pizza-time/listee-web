31ef076597ecfbde58436f6d62447bdb0ccc67e2
Alexander Bartolomey <occloxium@gmail.com>
2020-02-29 17:50:09 +0100

    Add dialogs for other fab button functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,75 @@
 <template>
   <div class="home">
     <StateDisplay></StateDisplay>
+    <v-dialog
+      v-model="clearDialog"
+      width="500"
+    >
+      <v-card>
+        <v-card-title
+          class="headline"
+          primary-title
+        >
+          Aktuelle Einkausliste wirklich löschen?
+        </v-card-title>
+        <v-card-text>
+          Durch das Bestätigen werden alle Zutaten auf der aktuellen Einkaufsliste gelöscht. <b>Die Liste wird nicht
+          archiviert und alle Daten sind unwideruflich verloren.</b> Neu gelernte Zutaten bleiben erhalten.
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            text
+            @click="clearDialog = false"
+          >
+            Abbrechen
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="error"
+            depressed
+            @click="clearList"
+          >
+            Löschen
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="nextDialog"
+      width="500"
+    >
+      <v-card>
+        <v-card-title
+          class="headline"
+          primary-title
+        >
+          Nächste Einkaufsliste eröffnen?
+        </v-card-title>
+        <v-card-text>
+          Durch das Bestätigen wird die aktuelle Liste archiviert eine neue leere Liste erstellt. Neu gelernte Zutaten bleiben erhalten.
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            text
+            @click="nextDialog = false"
+          >
+            Abbrechen
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="success"
+            depressed
+            @click="nextList"
+          >
+            Nächste Runde
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <List class="list"></List>
     <div id="create">
       <v-speed-dial
@@ -19,8 +88,11 @@
             </v-btn>
           </router-link>
         </template>
-        <v-btn color="red" fab small dark>
+        <v-btn color="red" fab small dark @click="showClearConfirmationDialog">
           <v-icon>delete_outline</v-icon>
+        </v-btn>
+        <v-btn color="green" fab small dark @click="showNextDialog">
+          <v-icon>keyboard_arrow_right</v-icon>
         </v-btn>
       </v-speed-dial>
     </div>
@@ -38,6 +110,8 @@ export default {
     StateDisplay
   },
   data: () => ({
+    clearDialog: false,
+    nextDialog: false,
     fab: false,
     fling: false,
     hover: true,
@@ -58,6 +132,23 @@ export default {
       }
     }
   },
+  methods: {
+    showClearConfirmationDialog(){
+      this.clearDialog = true;
+    },
+    showNextDialog(){
+      this.nextDialog = true;
+    },
+    clearList(){
+      this.$store.dispatch('list/resetList');
+      this.clearDialog = false;
+    },
+    nextList(){
+      // TODO
+      // this.$store.dispatch('list/resetList');
+      this.nextDialog = false;
+    },
+  }
 };
 </script>
 
